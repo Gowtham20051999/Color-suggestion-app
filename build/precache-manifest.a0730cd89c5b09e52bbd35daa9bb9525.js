1b1ce851ba26bba3dab4a46f2da84cf74bf8e96e
Gowtham20051999 <83176312+Gowtham20051999@users.noreply.github.com>
2022-09-07 23:41:45 +0530

    Add files via upload

---
+++ build/precache-manifest.a0730cd89c5b09e52bbd35daa9bb9525.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3f1641d868d436e21c35239783d0c8",
+    "url": "/Color-suggestion-app/index.html"
+  },
+  {
+    "revision": "f378ee5a4ad68187bdc1",
+    "url": "/Color-suggestion-app/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "a4fe69c1529445e89e9b",
+    "url": "/Color-suggestion-app/static/js/10.1f2a1c56.chunk.js"
+  },
+  {
+    "revision": "2214687501515a12a9cb",
+    "url": "/Color-suggestion-app/static/js/11.9036c2a3.chunk.js"
+  },
+  {
+    "revision": "0237ee5b2a3b2e59b6a5",
+    "url": "/Color-suggestion-app/static/js/2.08692190.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/Color-suggestion-app/static/js/2.08692190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35724371e577dfb53c95",
+    "url": "/Color-suggestion-app/static/js/3.95d897da.chunk.js"
+  },
+  {
+    "revision": "801a54b1e8688085acc1",
+    "url": "/Color-suggestion-app/static/js/4.e42b17b0.chunk.js"
+  },
+  {
+    "revision": "5cef80cc020f5a3b9448",
+    "url": "/Color-suggestion-app/static/js/5.84f28a63.chunk.js"
+  },
+  {
+    "revision": "ce4c89f47d511b60a8cf",
+    "url": "/Color-suggestion-app/static/js/6.23696afb.chunk.js"
+  },
+  {
+    "revision": "8dee8506575cd0c10a97",
+    "url": "/Color-suggestion-app/static/js/7.d0b03ef2.chunk.js"
+  },
+  {
+    "revision": "9173ff19ad7927d93cd1",
+    "url": "/Color-suggestion-app/static/js/8.a5cf0f08.chunk.js"
+  },
+  {
+    "revision": "537522a8a6afe4088f41",
+    "url": "/Color-suggestion-app/static/js/9.4f421de1.chunk.js"
+  },
+  {
+    "revision": "f378ee5a4ad68187bdc1",
+    "url": "/Color-suggestion-app/static/js/main.ae839a87.chunk.js"
+  },
+  {
+    "revision": "6e76bd6b79868e346010",
+    "url": "/Color-suggestion-app/static/js/runtime-main.cb13f3a9.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Color-suggestion-app/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/Color-suggestion-app/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
